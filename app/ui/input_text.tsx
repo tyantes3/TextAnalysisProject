8b6a7c069d93653a8f48bb635dce2e7836d7e1e0
BradyD33 <73864543+BradyD33@users.noreply.github.com>
2024-03-20 18:22:34 -0500

    change structure

--- app/ui/input_text.tsx
+++ app/ui/input_text.tsx
@@ -1,17 +1,17 @@
+
+'use client';
 export default function InputText() {
 
-  
-  async function queryModel(){
-    let model_text=document.getElementById("model_text")
-    console.log(model_text)
-    
-    let summary = await fetch("https://bba5-35-222-199-161.ngrok-free.app/api/queryModel",{
-      method:"GET",
-      headers: {
-        "Content-Type": "application/json"
+ 
 
-      },
-      body: JSON.stringify("Hello World")
+  async function queryModel(form){
+    form.preventDefault()
+    
+    let summary = await fetch("https://b2cd-35-196-52-77.ngrok-free.app//api/queryModel",{
+      method:"GET",
+      
+      // body: JSON.stringify("Hello World")
+      body: new FormData(form)
 
     })
     // let model_output = async await fetch(model_text,)
@@ -20,9 +20,9 @@ export default function InputText() {
 
 
     return (
-          <form>
-            <input className="text-white" type="text" name="model_text" id="mode_text"></input>
-            <button type='submit' id='submitQuery' onClick={queryModel}>Summarize</button>
+          <form id="query" onSubmit={queryModel} method="get">
+            <input className="text-white" type="text" name="model_text" id="model_text"></input>
+            <button type='submit' id='submitQuery' >Summarize</button>
           </form>
           
 
